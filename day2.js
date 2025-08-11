7e17a9cdae488ab757e00ec1364dee137c89499c
Ganeshjeyakumark <ganesh.ug.23.it@francisxavier.ac.in>
2025-08-11 15:02:47 +0530

    Add files via upload

---
+++ day2.js
@@ -0,0 +1,34 @@
+document.getElementById("registrationForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Stop form from refreshing page
+
+    let name = document.getElementById("name").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let department = document.getElementById("department").value.trim();
+    let year = document.getElementById("year").value.trim();
+    let projectTitle = document.getElementById("projectTitle").value.trim();
+
+    // Check all fields filled
+    if (!name || !email || !department || !year || !projectTitle) {
+        alert("Please fill all fields.");
+        return;
+    }
+
+    // Email format validation
+    let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailPattern.test(email)) {
+        alert("Please enter a valid email address.");
+        return;
+
+    }
+    // Check all fields filled
+if (!name || !email || !department || !year || !projectTitle) {
+    let form = document.querySelector(".container");
+    form.classList.add("error-shake");
+    setTimeout(() => form.classList.remove("error-shake"), 400);
+    alert("Please fill all fields.");
+    return;
+}
+
+
+    alert("Registration Successful!");
+});
